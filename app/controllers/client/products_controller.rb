b5a93db205426a854c824386e5c4d2727df6eda5
Tatiana Pena <Tatiana.c.pena1@gmail.com>
2018-09-27 21:31:04 -0500

    09.27.18

--- app/controllers/client/products_controller.rb
+++ app/controllers/client/products_controller.rb
@@ -1,8 +1,65 @@
 class Client::ProductsController < ApplicationController
   
   def index
-  response = HTTP.get("http://localhost:3000/api/products")
-  @products = response.parse
-  render 'index.html.erb'
+    response = HTTP.get("http://localhost:3000/api/products")
+    @products = response.parse
+    render 'index.html.erb'
   end
+
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    client_params = {
+                    name: params[:name],
+                    description: params[:description],
+                    price: params[:price],
+                    image_url: params[:image_url],
+                    supplier_id: params[:supplier_id]
+                    }
+
+    response = HTTP.post(
+                    "http://localhost:3000/api/products", 
+                    form: client_params
+                    )
+    @product = response.parse
+    redirect_to "/client/products/#{@product["id"]}"                  
+  end
+
+  def show
+    response = HTTP.get("http://localhost:3000/api/products/#{params[:id]}")
+    @product = response.parse
+    render 'show.html.erb'
+  end
+
+  def edit
+    response = HTTP.get("http://localhost:3000/api/products/#{params[:id]}")
+    @product = response.parse
+    render 'edit.html.erb'
+  end
+
+  def update
+    client_params = {
+                    name: params[:name],
+                    description: params[:description],
+                    price: params[:price],
+                    image_url: params[:image_url],
+                    supplier_id: params[:supplier_id]
+                    }
+
+    response = HTTP.patch(
+                          "http://localhost:3000/api/products/#{params[:id]}",
+                          form: client_params
+                          )
+
+    @recipe = response.parse
+    redirect_to "/client/products/#{ @product["id"] }"
+  end
+
+  def destroy
+    HTTP.delete("http://localhost:3000/api/products/#{params[:id]}")
+    redirect_to "/client/products"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   namespace :client do
     get "/products" => "products#index"
+    get "/products" => "products#new"
+    post "/products" => "products#create"
+    get '/products/:id' => 'products#show'
+    get '/products/:id/edit' => 'products#edit'
+    patch '/products/:id' => 'products#update'
+    delete '/products/:id' => 'products#destroy'
   end  
 end
